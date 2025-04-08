68922b07c24432a63af43005afec5e317dce5df4
EneaSema <sema.enea1993@gmail.com>
2025-04-08 16:04:20 +0200

    resolve-second-exercise

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,35 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+/** creazione funzione con un nuovo con le iniziali di ogni parola dell'array fornito
+ * @param {Array} oldArray vecchio array fonrito
+ * @param { Array} newArray restituzione del nuvo array con le inziali
+ */
+
+function searchByInitial(oldArray) {
+  const newArray = [];
+
+  let currentElement = "";
+
+  for (let i = 0; i < oldArray.length; i++) {
+    currentElement = oldArray[i];
+
+    for (let i = 0; i < currentElement.length; i++) {
+      let currentCharCompare = "";
+      currentCharCompare += currentElement[i];
+
+      if (currentCharCompare === currentElement[0]) {
+        newArray.push(currentCharCompare);
+      }
+    }
+  }
+  return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(searchByInitial(names));
 
-
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"];
