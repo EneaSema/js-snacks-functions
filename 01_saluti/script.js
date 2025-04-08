df76b7f9c3cf6ed9bc39917d8f78540daf3dc4d9
EneaSema <sema.enea1993@gmail.com>
2025-04-08 15:31:32 +0200

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,23 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = "Mario";
+console.log(userName);
+
+// Dichiara la funzione qui.
+
+/** creazione funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]")
+ * @param {string}  name valore iningresso che deve essere di tipo stringa
+ * @param {string}  outputText return di tipo stringa
+ */
+
+function hiSaluto(name) {
+  let outputText = "Ciao ";
+  outputText += name;
+  return outputText;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(hiSaluto(userName));
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
