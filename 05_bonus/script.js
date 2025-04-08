77ce9547b32437cc80d71a6fe008524e23651b3e
EneaSema <sema.enea1993@gmail.com>
2025-04-09 00:07:02 +0200

    start-five-exercise

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -6,9 +6,17 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = "Mario";
+console.log();
 
 // Dichiara la funzione qui.
-
+/**creo una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito.
+ * @param {string} parametrString parametro di stringa contentente un nome
+ * @param {string} resultString restituzione di una stringa contenente un saluto dal nome fonrito che cambia in base all'ora
+ */
+function messageHi(parametrString) {
+  return resultString;
+}
 // Invoca la funzione qui e stampa il risultato in console
+console.log(messageHi());
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
